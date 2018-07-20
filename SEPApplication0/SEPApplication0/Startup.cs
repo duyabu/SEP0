dbbf93debe3b6ca27f52a175cb61959e2fd47e55
duyabu <phmngcduy@yahoo.com>
2018-07-20 16:15:29 +0700

    empty website

---
+++ SEPApplication0/SEPApplication0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SEPApplication0.Startup))]
+namespace SEPApplication0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
